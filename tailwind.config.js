c847ac9e9b68837ece840f7ddec12159f6438d4a
daniel cespedes <daniel@DanielC-PC>
2022-10-25 21:56:53 -0500

    Inicializar proyecto, Config Eslint y Prettier, Integrando PostCSS y TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+  content: ['./src/**/*{html,js}'],
+  theme: {
+    colors: {
+      ...colors,
+    },
+  },
+};
